10fe00e24f618c07b9c3f4ceebff8376060416ad
Eric Chrobak <eric@ericchrobak.com>
2018-01-11 21:23:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,6 @@ var addElementToBeginningOfArray = function (array, n){
 }
 
 var destructivelyAddElementToBeginningOfArray = function (array, n){
-  var array1 = array;
-  array1.unshift(n);
-  return array1;
+  
+  return array1.unshift(n);
 }
